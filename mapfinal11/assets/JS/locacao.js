daae09ce09ae7b6a2160f7faeb58b3984d479696
Matheus <148652264+MatheusCauaCosta@users.noreply.github.com>
2024-10-09 14:23:29 -0300

    projeto atualmente

---
+++ mapfinal11/assets/JS/locacao.js
@@ -0,0 +1,478 @@
+const infoData = {
+    //locação
+
+    predioPrincipal: {
+        title: "Prédio Principal",
+        image: "assets/IMG/predioPrincipal.jpg"
+    },
+    anexo: {
+        title: "Anexo",
+        image: "https://media-cdn.tripadvisor.com/media/photo-s/0f/fb/9d/c4/lugar-lindo-paraiso-vista.jpg"
+    },
+    quadraCoberta: {
+        title: "Quadra Coberta",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5RKfn_46BEkwfszsniINRwX7t9Q1esafrMQ&s"
+    },
+    quadrinha: {
+        title: "Quadra aberta",
+        image: "https://th.bing.com/th/id/R.d9f040798931c98df0d8a952ada765db?rik=uSRaTW9kDDVDBg&pid=ImgRaw&r=0"
+    },
+
+    //anexo
+
+    labCadDeGestao: {
+        title: "labCadDeGestão",
+        image: ""
+    },
+
+    sala1A: {
+        title: "sala1A",
+        image: ""
+    },
+
+    sala2A: {
+        title: "sala2A",
+        image: ""
+    },
+
+    sala3A: {
+        title: "sala3A",
+        image: ""
+    },
+
+    sala4A: {
+        title: "sala4A",
+        image: ""
+    },
+
+    labCadOperacional: {
+        title: "labCadOperacional",
+        image: ""
+    },
+
+    labCadAmbiente01: {
+        title: "labCadAmbiente01",
+        image: ""
+    },
+
+    labCadAmbiente02: {
+        title: "labCadAmbiente02",
+        image: ""
+    },
+
+    labCadAmbiente03: {
+        title: "labCadAmbiente03",
+        image: ""
+    },
+
+    labCadAmbiente04: {
+        title: "labCadAmbiente04",
+        image: ""
+    },
+
+    biblioteca: {
+        title: "biblioteca",
+        image: ""
+    },
+
+    sala5A: {
+        title: "sala5A",
+        image: ""
+    },
+
+    sala6A: {
+        title: "sala6A",
+        image: ""
+    },
+
+    sala7A: {
+        title: "sala7A",
+        image: ""
+    },
+
+    sala8A: {
+        title: "sala8A",
+        image: ""
+    },
+
+    arquivoMorto: {
+        title: "arquivoMorto",
+        image: ""
+    },
+
+    sala9A: {
+        title: "sala9A",
+        image: ""
+    },
+
+    //superior
+
+    sala1S: {
+        title: "sala1S",
+        image: ""
+    },
+    sala2S: {
+        title: "sala2S",
+        image: ""
+    },
+    sala3S: {
+        title: "sala3S",
+        image: ""
+    },
+    sala4S: {
+        title: "sala4S",
+        image: ""
+    },
+    sala5S: {
+        title: "sala5S",
+        image: ""
+    },
+    topografia: {
+        title: "topografia",
+        image: ""
+    },
+    sala6S: {
+        title: "sala6S",
+        image: ""
+    },
+    sala7S: {
+        title: "sala7S",
+        image: ""
+    },
+    salaDesenho01: {
+        title: "salaDesenho01",
+        image: ""
+    },
+    salaDesenho02: {
+        title: "salaDesenho02",
+        image: ""
+    },
+    salaDesenho03: {
+        title: "salaDesenho03",
+        image: ""
+    },
+    salaDesenho04: {
+        title: "salaDesenho04",
+        image: ""
+    },
+    salaDesenho05: {
+        title: "salaDesenho05",
+        image: ""
+    },
+    pátio: {
+        title: "pátio",
+        image: ""
+    },
+    turismo: {
+        title: "turismo",
+        image: ""
+    },
+
+    //térreo
+
+    sala1T: {
+        title: "sala1T",
+        image: ""
+    },
+    sala2T: {
+        title: "sala2T",
+        image: ""
+    },
+    sala3T: {
+        title: "sala3T",
+        image: ""
+    },
+    salaMaker: {
+        title: "salaMaker",
+        image: ""
+    },
+    cozinha: {
+        title: "cozinha",
+        image: ""
+    },
+    salaAmbiente01: {
+        title: "salaAmbiente01",
+        image: ""
+    },
+    salaAmbiente02: {
+        title: "salaAmbiente02",
+        image: ""
+    },
+    salaAmbiente03: {
+        title: "salaAmbiente03",
+        image: ""
+    },
+    salaAmbiente04: {
+        title: "salaAmbiente04",
+        image: ""
+    },
+    sala4T: {
+        title: "sala4T",
+        image: ""
+    },
+    labInformatica: {
+        title: "labInformatica",
+        image: ""
+    },
+    labQuimica: {
+        title: "labQuimica",
+        image: ""
+    },
+    deposito02: {
+        title: "deposito02",
+        image: ""
+    },
+    labFisica: {
+        title: "labFisica",
+        image: ""
+    },
+    almoxarifado: {
+        title: "almoxarifado",
+        image: ""
+    },
+    orientadoraEducacional: {
+        title: "orientadoraEducacional",
+        image: ""
+    },
+    salaPortugues: {
+        title: "salaPortugues",
+        image: ""
+    },
+    secaoPessoal: {
+        title: "secaoPessoal",
+        image: ""
+    },
+    salaProfessores: {
+        title: "salaProfessores",
+        image: ""
+    },
+    portaria: {
+        title: "portaria",
+        image: ""
+    },
+    deposito01: {
+        title: "deposito01",
+        image: ""
+    },
+    secretaria01: {
+        title: "secretaria01",
+        image: ""
+    },
+    admSecretaria: {
+        title: "admSecretaria",
+        image: ""
+    },
+    secretaria02: {
+        title: "secretaria02",
+        image: ""
+    },
+    setorEstagio01: {
+        title: "setorEstagio01",
+        image: ""
+    },
+    coordPedag: {
+        title: "coordPedag",
+        image: ""
+    },
+    setorEstagio02: {
+        title: "setorEstagio02",
+        image: ""
+    },
+    grafica: {
+        title: "grafica",
+        image: ""
+    },
+    secretaria03: {
+        title: "secretaria03",
+        image: ""
+    },
+    patio: {
+        title: "patio",
+        image: "assets/IMG/predioPrincipal.jpg"
+    },
+    diretoraServ: {
+        title: "diretoraServ",
+        image: ""
+    },
+    diretora: {
+        title: "diretora",
+        image: ""
+    },
+    manutencao: {
+        title: "manutencao",
+        image: ""
+    },
+    apm: {
+        title: "apm",
+        image: ""
+    },
+    centroMemoria: {
+        title: "centroMemoria",
+        image: ""
+    },
+    multimeios: {
+        title: "multimeios",
+        image: ""
+    },
+    deposito03: {
+        title: "deposito03",
+        image: ""
+    },
+    salaIngles: {
+        title: "salaIngles",
+        image: ""
+    },
+
+    //inferior 1
+    banheiroMasculino1: {
+        title: "banheiroMasculino1",
+        image: ""
+    },
+    banheiroFeminino2: {
+        title: "banheiroFeminino2",
+        image: ""
+    },
+    jardim1: {
+        title: "jardim1",
+        image: ""
+    },
+    jardim2: {
+        title: "jardim2",
+        image: ""
+    },
+    sala1I1: {
+        title: "sala1I1",
+        image: ""
+    },
+    sala2I1: {
+        title: "sala2I1",
+        image: ""
+    },
+    sala3I1: {
+        title: "sala3I1",
+        image: ""
+    },
+    sala4I1: {
+        title: "sala4I1",
+        image: ""
+    },
+    sala5I1: {
+        title: "sala5I1",
+        image: ""
+    },
+    sala6I1: {
+        title: "sala6I1",
+        image: ""
+    },
+    sala7I1: {
+        title: "sala7I1",
+        image: ""
+    },
+    depositoLimpeza: {
+        title: "depositoLimpeza",
+        image: ""
+    },
+    banheiroMasculinoC: {
+        title: "banheiroMasculinoC",
+        image: ""
+    },
+    banheiroFemininoC: {
+        title: "banheiroFemininoC",
+        image: ""
+    },
+    dispensa: {
+        title: "dispensa",
+        image: ""
+    },
+    cantina: {
+        title: "cantina",
+        image: ""
+    },
+    cantinaRefereitorio: {
+        title: "cantinaRefereitorio",
+        image: ""
+    },
+    depositoAlimento: {
+        title: "depositoAlimento",
+        image: ""
+    },
+    depositoI1: {
+        title: "depositoI1",
+        image: ""
+    },
+    segurancaTrabalho: {
+        title: "segurancaTrabalho",
+        image: ""
+    },
+    manutencao: {
+        title: "manutencao",
+        image: ""
+    },
+    materialEsportivo: {
+        title: "materialEsportivo",
+        image: ""
+    },
+    banheiroFeminio1: {
+        title: "banheiroFeminio1",
+        image: ""
+    },
+    sala8I1: {
+        title: "sala8I1",
+        image: ""
+    },
+    patioI1: {
+        title: "patioI1",
+        image: ""
+    },
+    areaLuz: {
+        title: "areaLuz",
+        image: ""
+    },
+
+    //inferior 2
+
+    sala1I2: {
+        title: "sala1I2",
+        image: ""
+    },
+    sala2I2: {
+        title: "sala2I2",
+        image: ""
+    },
+    pneumatica: {
+        title: "pneumatica",
+        image: ""
+    },
+    anfiteatro: {
+        title: "anfiteatro",
+        image: ""
+    },
+    oficinaMecanica: {
+        title: "oficinaMecanica",
+        image: ""
+    }
+};
+
+function shownewbar(area) {
+    const infoKey = area.getAttribute('data-info');
+    const info = infoData[infoKey];
+    
+    if (info) {
+        const newbar = document.getElementById('newbar');
+        newbar.innerHTML = `
+            <div class="newbar" style="display: flex; flex-direction: column;">
+                <h2>${info.title}</h2>
+                <img class="newimg" src="${info.image}" alt="${info.title}">
+            </div>
+        `;
+        newbar.style.display = 'flex';
+    }
+}
+
+// Adicionar listeners de evento para todas as áreas
+document.querySelectorAll('area').forEach(area => {
+    area.addEventListener('click', () => shownewbar(area));
+    area.addEventListener('keypress', (e) => {
+        if (e.key === 'Enter') shownewbar(area);
+    });
+});
